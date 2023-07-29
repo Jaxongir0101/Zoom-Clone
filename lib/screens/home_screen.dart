6da945831c1df82e21c6cb6ab93f191b459d18f7
JaxongirDev <rustamovjaxongir1@gmail.com>
2023-07-30 00:43:27 +0500

    feat:Zoom Home Screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zoom_clone/utils/colors.dart';
+import 'package:zoom_clone/widgets/home_meeting_button.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,11 +10,82 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _page = 0;
+
+  onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-
+        title: const Text("Meet & Chat"),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              HomeMeetingButton(
+                onPressed: () {},
+                icon: Icons.videocam,
+                text: "New Meeting",
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                icon: Icons.add_box_rounded,
+                text: "Join Meeting",
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                icon: Icons.calendar_today,
+                text: "Schedule",
+              ),
+              HomeMeetingButton(
+                onPressed: () {},
+                icon: Icons.arrow_upward_rounded,
+                text: "Share Screen",
+              ),
+            ],
+          ),
+          const Expanded(
+              child: Center(
+            child: Text(
+              "Create/Join Meetings with just a click!",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ))
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: footerColor,
+        selectedItemColor: Colors.white,
+        currentIndex: _page,
+        type: BottomNavigationBarType.fixed,
+        onTap: onPageChanged,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: "Meet & Chat"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.lock_clock), label: "Meetings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline), label: "Contacts"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.comment_bank), label: "Meet & Chat "),
+        ],
       ),
     );
   }
